29ab3e9d192b43e9b22e8bfb7ce986b9c962a42a
min.tian <tianmin@pku.edu.cn>
2021-04-13 16:33:42 +0800

    ..

--- src/Views/Header/index.js
+++ src/Views/Header/index.js
@@ -1,15 +1,39 @@
 import React from "react";
+import { Upload, Button } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 import "./index.scss";
 import userList from "../../Common/userList";
 import { useGlobalStore } from "../../Store";
 import { observer } from "mobx-react-lite";
 import { Select } from "antd";
+import { baseUrl } from "../../Server";
+
 const { Option } = Select;
 
 const Header = observer(() => {
   const store = useGlobalStore();
   const handleChange = (userId) => {
     store.setUserId(userId);
+  };
+  const url = baseUrl + "add_papers";
+  const props = {
+    action: url,
+    // customRequest: (d, t, g) => {
+    //   console.log("d", d);
+    //   console.log("t", t);
+    //   console.log("g", g);
+    // },
+    beforeUpload: (file, fileList) => {
+      console.log("===> file", file);
+      console.log("===> fileList", fileList);
+      // store.update(file)
+      return true;
+    },
+    onChange: (info) => {
+      console.log("===>info", info);
+      console.log(info.fileList);
+    },
+    itemRender: () => <></>,
   };
   return (
     <div className="header">
@@ -29,6 +53,11 @@ const Header = observer(() => {
             </Option>
           ))}
         </Select>
+        <Upload {...props}>
+          <Button className="add-data" icon={<UploadOutlined />} size="small">
+            Upload
+          </Button>
+        </Upload>
       </div>
     </div>
   );
